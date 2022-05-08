11f24ea60d87758413eff1770921c0acb56257c7
VirtuuX <somargame22@gmail.com>
2022-05-08 18:14:46 +0600

    Updates

---
+++ precache-manifest.e81acdef5a5f0f1eec868388adcc12ff.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b5abdd68853dd0417d513fcf07d71a",
+    "url": "/Social-Network/index.html"
+  },
+  {
+    "revision": "539f005514cf27a6d6dd",
+    "url": "/Social-Network/static/css/4.c0be6f51.chunk.css"
+  },
+  {
+    "revision": "7639995d530ee58ac65e",
+    "url": "/Social-Network/static/css/5.b9216d0b.chunk.css"
+  },
+  {
+    "revision": "dcf711a611a7d3d56723",
+    "url": "/Social-Network/static/css/6.30f6f024.chunk.css"
+  },
+  {
+    "revision": "2e3e5ae7580bd190bc17",
+    "url": "/Social-Network/static/css/7.1419702e.chunk.css"
+  },
+  {
+    "revision": "be76be7efd2ae91e9f01",
+    "url": "/Social-Network/static/css/main.01cfb7bd.chunk.css"
+  },
+  {
+    "revision": "13e840b9ed8556d377f6",
+    "url": "/Social-Network/static/js/0.fac6cabf.chunk.js"
+  },
+  {
+    "revision": "d40a85704f0ee939319d",
+    "url": "/Social-Network/static/js/3.c000a116.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social-Network/static/js/3.c000a116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539f005514cf27a6d6dd",
+    "url": "/Social-Network/static/js/4.81a2e8fc.chunk.js"
+  },
+  {
+    "revision": "7639995d530ee58ac65e",
+    "url": "/Social-Network/static/js/5.c77fba57.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/Social-Network/static/js/5.c77fba57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf711a611a7d3d56723",
+    "url": "/Social-Network/static/js/6.e604e29d.chunk.js"
+  },
+  {
+    "revision": "2e3e5ae7580bd190bc17",
+    "url": "/Social-Network/static/js/7.5cc7f6d3.chunk.js"
+  },
+  {
+    "revision": "be76be7efd2ae91e9f01",
+    "url": "/Social-Network/static/js/main.fd179de8.chunk.js"
+  },
+  {
+    "revision": "80c4723f9e9754153968",
+    "url": "/Social-Network/static/js/runtime-main.c4a0dd2e.js"
+  },
+  {
+    "revision": "3f907dc3e1a06faea862c95e05b2f3b0",
+    "url": "/Social-Network/static/media/ava.3f907dc3.jpg"
+  },
+  {
+    "revision": "0690916617a92e9b65db1a1335b811f6",
+    "url": "/Social-Network/static/media/ava2.06909166.jpg"
+  },
+  {
+    "revision": "3de431ecc3f0d0e1ab6648f90e477548",
+    "url": "/Social-Network/static/media/bg.3de431ec.jpg"
+  },
+  {
+    "revision": "79439d34ef1509f1871fb8e275d90831",
+    "url": "/Social-Network/static/media/logo.79439d34.png"
+  },
+  {
+    "revision": "8f68e1b53ba9403bedfe1df49bd74924",
+    "url": "/Social-Network/static/media/logo2.8f68e1b5.jpg"
+  },
+  {
+    "revision": "11e903c4cd56d12655a68b00af84fc01",
+    "url": "/Social-Network/static/media/preloader.11e903c4.gif"
+  },
+  {
+    "revision": "8f68e1b53ba9403bedfe1df49bd74924",
+    "url": "/Social-Network/static/media/virtuux.8f68e1b5.jpg"
+  }
+]);
